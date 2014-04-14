b168048ee61d98283b0c94ba09fc62260b52fcad
DennisBuijs <dennisdennisbuijs@gmail.com>
2014-04-14 10:09:35 +0200

    toevoegen Bootstrap
    Toevoegen van Bootstrap CSS- en JS-files.

--- views/index/header.php
+++ views/index/header.php
@@ -1,8 +1,18 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title><?= $this->seo['title'] ?></title>    
+
+    <title>
+    	<?= $this->seo['title'] ?>
+    </title> 
+
+		<link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/css/style.css">
+
   </head>
-  <h1>Questionmark</h1>
+
+  <body>
+
+  	<h1>Questionmark?</h1>
   
     
