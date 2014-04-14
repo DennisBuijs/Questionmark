b4f16aa93e2ee456fb278f18f897ac0a6e13c87d
Dennis van Otterloo <dennisvanotterloo@gmail.com>
2014-04-14 10:18:22 +0200

    Verplaatsen footer en header

---
+++ views/footer.php
@@ -0,0 +1,10 @@
+		<footer>
+		  wow footer 
+		</footer>
+
+		<script src="public/js/jquery.js"></script>
+		<script src="public/js/bootstrap.min.js"></script>
+
+	</body>
+
+</html>
---
+++ views/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+    <title>
+    	<?= $this->seo['title'] ?>
+    </title> 
+
+		<link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/css/style.css">
+
+  </head>
+
+  <body>
+
+  	<h1>Questionmark?</h1>
+  
+    
