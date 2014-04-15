c6263448dac32cceb76d4095f53356066e9ead0a
DennisBuijs <dennisdennisbuijs@gmail.com>
2014-04-15 13:29:50 +0200

    Toevoegen show_alert functie

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,28 @@
 $(document).ready(function() {
 
+  // This function shows an alert
+  // type: the type of alert, has the be one of: succes, info, warning, danger
+  // message: the message the alert will say
+  show_alert = function(type, message) {
+
+    $(".alert-fixed-top").remove();
+    $("body").prepend("<div class=\"alert alert-" + type + " alert-dismissable alert-fixed-top\"><button type=\"button\" class=\"close close-alert-fixed-top\" aria-hidden=\"true\">&times;</button>" + message + "</div>");
+    $(".alert-fixed-top").slideDown();
+
+    $(".close-alert-fixed-top").on("click", function() {
+
+      $(".alert-fixed-top").slideUp();
+
+    });
+
+  }
+
   $('form').on('submit', function() {
     var iNumOfInvalidValues = 0;
     $('[data-required=required]').each(function() {
       var $this = $(this);
 
       if (this.value === '') {
-        $("body").append("DINGEN KLOPPEN NIET");
         $this.parent().addClass('has-error');
 
         iNumOfInvalidValues++;
@@ -19,7 +35,10 @@ $(document).ready(function() {
     });
 
     if (iNumOfInvalidValues !== 0)
+
+      show_alert("danger", "De logingegevens zijn niet correct.");
       return false;
+
   });
 
 
