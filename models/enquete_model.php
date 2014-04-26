6bcf07598de0d810d1574541bce3f58a5492154a
Dennis van Otterloo <dennisvanotterloo@gmail.com>
2014-04-26 14:17:25 +0200

    Edit functionaliteit in Enquete Class maken

--- models/enquete_model.php
+++ models/enquete_model.php
@@ -4,12 +4,32 @@ class Enquete_Model extends Model {
 
   public function get_enquete_by_id($id) {
     return Enquete::get_enquete_by_id($id);
-    
   }
 
 
   public function delete($id) {
     Enquete::delete($id);
+  }
+
+
+  public function edit() {
+
+
+    // JSON word ingeladen vanuit frontend
+    $json = file_get_contents("http://localhost:8888/Questionmark/public/js/edit.json");
+    $enquete = json_decode($json, true);
+
+    $name = $enquete['name'];
+    $introduction = $enquete['introduction'];
+    $start_date = "2014-04-15" /* $enquete['start_date'] */;
+    $end_date = "2014-04-15" /* $enquete['end_date'] */;
+    $questions = $enquete['questions'];
+    $deleted_questions = $enquete['deleted_question'];
+    $deleted_attributes = $enquete['deleted_attributes'];
+    $enquete_id = $enquete['id'];
+    $user_id = $_SESSION['user_id'];
+
+    Enquete::edit($name, $introduction, $start_date, $end_date, $questions, $deleted_questions, $deleted_attributes, $enquete_id, $user_id);
   }
 
 
