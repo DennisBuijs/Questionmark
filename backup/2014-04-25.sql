56dceea9a62a9c8219cca654af793e7ec68e557b
Dennis van Otterloo <dennisvanotterloo@gmail.com>
2014-04-26 14:15:57 +0200

    Toevoegen SQL file

---
+++ backup/2014-04-25.sql
@@ -0,0 +1,153 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.8
+-- http://www.phpmyadmin.net
+--
+-- Machine: localhost:8889
+-- Gegenereerd op: 25 apr 2014 om 23:48
+-- Serverversie: 5.5.34
+-- PHP-versie: 5.5.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Databank: `questionmark`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `Answers`
+--
+
+CREATE TABLE `Answers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Questions_id` int(11) NOT NULL,
+  `Sessions_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `Enquetes`
+--
+
+CREATE TABLE `Enquetes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `introduction` text,
+  `creation_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `Users_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `Questions`
+--
+
+CREATE TABLE `Questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question` varchar(255) DEFAULT NULL,
+  `order` int(11) NOT NULL,
+  `required` tinyint(1) NOT NULL DEFAULT '0',
+  `Question_Types_id` int(11) NOT NULL,
+  `Enquetes_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `Question_Attributes`
+--
+
+CREATE TABLE `Question_Attributes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `attribute` varchar(255) DEFAULT NULL,
+  `Questions_id` int(11) NOT NULL,
+  `Question_Attribute_Types_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `Question_Attribute_Types`
+--
+
+CREATE TABLE `Question_Attribute_Types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `attribute_type` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `Question_Attribute_Types`
+--
+
+INSERT INTO `Question_Attribute_Types` (`id`, `attribute_type`) VALUES
+(1, 'option'),
+(2, 'placeholder');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `Question_Types`
+--
+
+CREATE TABLE `Question_Types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `Question_Types`
+--
+
+INSERT INTO `Question_Types` (`id`, `type`) VALUES
+(1, 'textfield'),
+(2, 'textarea'),
+(3, 'radio'),
+(4, 'select'),
+(5, 'checkbox');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `Sessions`
+--
+
+CREATE TABLE `Sessions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `Users`
+--
+
+CREATE TABLE `Users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `Users`
+--
+
+INSERT INTO `Users` (`id`, `name`, `password`, `email`) VALUES
+(1, 'admin', 'db3ccd27b8cff3dd070e7e56ae33e7bcc83536118f1240af0a4688c4d7216474e01f3c3cc5f9b86df15c591120e4d43ffdf2bbf7b4e409c4d78dba20712daabc', 'admin@gmail.com');
